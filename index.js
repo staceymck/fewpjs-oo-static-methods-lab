272190aaf69a7ee7e1c6f0803b5e6bdc340ecfa8
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-24 10:05:01 -0500

    commit tree b0542178aa6f7948f78775c4b8f03f46bdf5d68d

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    const words = string.split(' ');
+    const titleCase = words.map((word) => {
+      if (!exceptions.includes(word) || word === words[0]) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }; 
+    });
+    return titleCase.join(" ");
+  }
 }
